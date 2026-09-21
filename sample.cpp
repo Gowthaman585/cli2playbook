79ac066c4653583c8c668a7e3fe79a4fc64b497f
Gowthaman M <gowthamanm585@gmail.com>
2026-09-21 10:28:13 +0530

    feat: add podman run parser for container name, image, and command

--- sample.cpp
+++ sample.cpp
@@ -2,11 +2,49 @@
 #include<iostream>
 #include<string>
 
+void call_run_container(std::vector<std::string>& vec){
+	std::string container_name;
+	std::string image_name;
+	std::vector<std::string> commands;
+
+	size_t i= 2;
+
+	while(i< vec.size()){
+		if(vec[i] == "--name"&& i+1<vec.size()){
+			container_name= vec[++i];
+		}
+		else if(image_name.empty()){
+			image_name= vec[i];
+		}
+		else{
+			commands.push_back(vec[i]);
+		}
+		++i;
+	}
+
+	std::cout<<"- name: run container\n";
+	std::cout<<"  containers.podman.podman_container:\n";
+	std::cout<<"	image: "<<image_name<<"\n";
+	
+	if(!container_name.empty()){
+		std::cout<<"	name: "<<container_name<<"\n";
+	}
+
+	if(!commands.empty()){
+		std::cout<<"	command: ";
+		size_t t = 0;
+		while(t<commands.size()){
+			std::cout<<commands[t]<<(t+1 < commands.size()?" ":"");
+			++t;
+		}
+	std::cout<<std::endl;
+	}
+}
 int main(int argc,char* argv[]){
 	std::vector<std::string> args(argv+1,argv + argc);
-	for(int i =0;i<args.size();i++){
-		std::cout<<args[i]<<" ";
-	}
 	
+	if(args.size() >=3 && args[0] == "podman" && args[1] == "run"){
+			call_run_container(args);
+	}
 	return 0;
 }
